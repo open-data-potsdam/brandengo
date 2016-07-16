cbe77728af63f6a6adfaf341e213cfbb426df924
Johannes Filter <hi@jfilter.de>
2016-07-16 14:55:28 +0200

    polyfil

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  entry: './app/app.jsx',
+  entry: ['whatwg-fetch', 'babel-polyfill','./app/app.jsx'],
   output: { path: __dirname, filename: 'app/bundle.js' },
   module: {
     loaders: [
